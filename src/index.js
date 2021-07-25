67bee66b4cec83030f064f46bbc1fb127fab9e50
Oleksandr Cherkasov <alekscherkasov13@gmail.com>
2021-07-25 16:04:28 +0300

    fill; human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+
+    if (number > 99) {
+        return convert_hundreds(number).trim()
+    }
+    else if (number === 0) {
+        return convert(number).trim()
+    } else {
+        return convert_tens(number).trim()
+    }
+
+
+    function convert_hundreds(num) {
+        if (num > 99) {
+            return ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100);
+        } else {
+            return convert_tens(num);
+        }
+    }
+
+    function convert_tens(num) {
+        if (num < 10) return ones[num];
+        else if (num >= 10 && num < 20) return teens[num - 10];
+        else {
+            return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+        }
+    }
+
+    function convert(num) {
+        if (num == 0) return "zero";
+        else return convert_millions(num);
+    }
 }
+
